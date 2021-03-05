3712fbc7d4e2bc2add20d47fccef745cb07ec298
YukYuPhat <76461000+YukYuPhat@users.noreply.github.com>
2021-03-05 11:04:29 -0600

    Added some sleepy time to slow things down

--- ciscoWebReport.py
+++ ciscoWebReport.py
@@ -15,6 +15,7 @@ websiteThing = 'https://10.1.100.100/login?CSRFKey=7658bc1b-a22a-8c3f-66da-626c4
 whitelistLoc = "https://10.1.100.100/prod/web/10_0/web_security_manager/custom_policy_elements/custom_url_categories"
 bypasslistLoc = 'https://10.1.100.100/prod/web/10_0/web_security_manager/global_settings/bypass_proxy'
 myPassword = whatPasswd('CiscoWebApp')
+timeUp = 10
 
 if __name__ == '__main__':
 
@@ -34,7 +35,7 @@ if __name__ == '__main__':
     browser.get(websiteThing)
 
     wait = WebDriverWait( browser, 5)
-
+    time.sleep(timeUp)
     #find user name
     usernameBox = browser.find_element_by_xpath('//*[@id="table_form_login"]/tbody/tr[1]/td/input')
 
@@ -52,62 +53,68 @@ if __name__ == '__main__':
     submit.click()
     #wait for the browser to load
     wait = WebDriverWait( browser, 15)
+    time.sleep(timeUp)
 
     #get page title and ensure we are in
 
-    pageTitle = browser.title
-    assert (pageTitle == 'Cisco Content Security Management Virtual Appliance M100V (10.1.100.100) - Centralized Services > System Status')
+#    pageTitle = browser.title
+#    assert (pageTitle == 'Cisco Content Security Management Virtual Appliance M100V (10.1.100.100) - Centralized Services > System Status')
 
     #wait = WebDriverWait( browser, 15)
     # goto the whitelist table
     browser.get(whitelistLoc)
 
     wait = WebDriverWait( browser, 15)
+    time.sleep(timeUp)
 
     #click on the Whitelist URL
     submit = browser.find_element_by_xpath('//*[@id="form"]/dl/dd/table/tbody/tr[3]/td[2]/a')
     submit.click()
 
     wait = WebDriverWait( browser, 15)
+    time.sleep(timeUp)
 
     #copy the whitelist
     whiteList = browser.find_element_by_xpath('//*[@id="category_urls"]').text
 
     #write it to the txt file
-    f.write('Whitelist is below:')
-    f.write(whiteList)
+    f.write('Whitelist is below:\n')
+    f.write(whiteList+'\n')
 
     #go back to the whitelist table
     browser.get(whitelistLoc)
 
     wait = WebDriverWait( browser, 15)
+    time.sleep(timeUp)
 
     #click on the Blacklist URL
     submit = browser.find_element_by_xpath('//*[@id="form"]/dl/dd/table/tbody/tr[6]/td[2]/a')
     submit.click()
 
     wait = WebDriverWait( browser, 15)
+    time.sleep(timeUp)
 
     #copy the Blacklist
     blackList = browser.find_element_by_xpath('//*[@id="category_urls"]').text
 
     #write it to the txt file
-    f.write("Blacklist is below:")
-    f.write(blackList)
+    f.write("Blacklist is below:\n")
+    f.write(blackList+'\n')
 
     #go to the bypasslist table
     browser.get(bypasslistLoc)
 
     wait = WebDriverWait( browser, 15)
+    time.sleep(timeUp)
 
-
-
-    #copy the bypasslist
-    bypassList = browser.find_element_by_xpath('//*[@id="category_urls"]').text
+    #copy the bypasslist 
+    bypassList = browser.find_element_by_xpath('//*[@id="form"]/dl/dd/table/tbody/tr/td').text
 
     #write it to the txt file
-    f.write("bypasslist is below:")
-    f.write(bypassList)
+    f.write("bypasslist is below:\n")
+    f.write(bypassList+'\n')
 
     browser.quit()
     f.close()
+    
+    
